87417f20535c19c1213fbde8c36be47b3f15f2f4
Marcin <marcin.macina@outlook.com>
2022-12-19 15:01:10 +0100

    Initial commit

---
+++ src/test/java/com/crud/mmac_library/MMacLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.mmac_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MMacLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
